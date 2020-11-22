5f8b60720ca4f3dbf73a9368b5f76eabfc8d6466
akmaralnassir <akmaralnassir@gmail.com>
2020-11-22 15:04:49 +0300

    functions of bullet

--- game_functions.py
+++ game_functions.py
@@ -3,20 +3,23 @@ import pygame
 import random
 from pygame import mixer
 
-def check_keydown_events(event,ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+    	new_bullet = Bullet(ai_settings, screen, ship)
+    	bullets.add(new_bullet)
 
-def check_keyup_events(event, ship):
+def check_keyup_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -25,6 +28,8 @@ def check_events(ship):
             check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
+        elif event.type == pygame.KEYDOWN:
+        	check_keydown_events(event, ai_settings, screen, ship, bullets)
             
 def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
@@ -41,4 +46,6 @@ def update_screen(ai_settings, screen, ship, bullets):
     screen.fill((0,0,0))
     screen.blit(ai_settings.bg, (0, 0))
     ship.blitme()
+    for bullet in bullets.sprites():
+    	bullet.draw_bullet()
     pygame.display.flip() 
