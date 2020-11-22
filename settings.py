a681f6c84da8a78894da2fce031134e99fac22ec
Your Name <you@example.com>
2020-11-22 14:05:01 +0300

    bullet settings

--- settings.py
+++ settings.py
@@ -4,3 +4,7 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 670
         self.bg = pygame.image.load('images/space.jpg')
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
