f4263c25c980ffc0e31e6156b868831a27b44e0a
HP <armirrorww@gmail.com>
2020-11-23 02:30:31 +0600

    rows

--- game_functions.py
+++ game_functions.py
@@ -69,22 +69,29 @@ def fire_bullet(ai_settings, screen, ship, bullets):
 
 def create_fleet(ai_settings, screen, ship, aliens):
     alien = Chicken(ai_settings, screen)
-    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)    for alien_number in range(number_aliens_x):
-        alien = Chicken(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
-    for alien_number in range(number_aliens_x):
-        create_alien(ai_settings, screen, aliens, alien_number)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+
+
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number,row_number)
 def get_number_aliens_x(ai_settings, alien_width):
 
     available_space_x = ai_settings.screen_width - 2 * alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
 
     return number_aliens_x
-def create_alien(ai_settings, screen, aliens, alien_number):
-    alien = Alien(ai_settings, screen)
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+    alien = Chicken(ai_settings, screen)
     alien_width = alien.rect.width
     alien.x = alien_width + 2 * alien_width * alien_number
     alien.rect.x = alien.x
     aliens.add(alien)
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    available_space_y = (ai_settings.screen_height -(3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
