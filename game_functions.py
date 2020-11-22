041c681f55d74458c582c5f66bc1de285c45ead4
akmaralnassir <akmaralnassir@gmail.com>
2020-11-22 14:50:22 +0300

    adding bullets

--- game_functions.py
+++ game_functions.py
@@ -1,8 +1,9 @@
 import sys
-
 import pygame
+import random
+from pygame import mixer
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event,ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
@@ -22,16 +23,22 @@ def check_events(ship):
 
         elif event.type == pygame.KEYDOWN:
             check_keydown_events(event, ship)
-        
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event,ship)
+            
+def check_events(ai_settings, screen, ship, bullets):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit() 
+            
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-
-
-def update_screen(ai_settings, screen, ship):
-        screen.fill((0,0,0))
-        screen.blit(ai_settings.bg, (0, 0))
-        ship.blitme()
-        pygame.display.flip()
-
+def update_screen(ai_settings, screen, ship, bullets):
+    screen.fill((0,0,0))
+    screen.blit(ai_settings.bg, (0, 0))
+    ship.blitme()
+    pygame.display.flip() 
