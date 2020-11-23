37fc300ffc3609f2c7a365aa0ca51c0a5492ceb0
Aknur <aknurtemir9@gmail.com>
2020-11-23 17:16:01 +0500

    game

--- me.py
+++ me.py
@@ -1,14 +1,18 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship():
+class Ship(Sprite):
     def __init__(self, ai_settings, screen):
+        super(Ship, self).__init__()
         self.screen = screen
         self.ai_settings=ai_settings
+
         self.image = pygame.image.load('images/spaceship.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
+
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.bottom = self.screen_rect.bottom - 5
         self.center = float(self.rect.centerx)
 
         self.moving_right = False
@@ -22,5 +26,10 @@ class Ship():
 
         self.rect.centerx = self.center
 
+    def center_ship(self):
+		self.center = self.screen_rect.centerx
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
+
