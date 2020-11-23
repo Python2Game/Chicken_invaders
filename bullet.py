abb0625fdaa71b372eaf2f490c273b96fbc4c780
akmaralnassir <akmaralnassir@gmail.com>
2020-11-23 18:04:06 +0300

    enemy bullets

--- bullet.py
+++ bullet.py
@@ -14,9 +14,35 @@ class Bullet(Sprite):
         self.y = float(self.rect.y)
         self.speed_factor = ai_settings.bullet_speed_factor
 
-
     def update(self):
         self.y -= self.speed_factor
         self.rect.y = self.y
     def draw_bullet(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
+
+class Enemy_Bullet(Sprite):
+
+	def __init__(self, ai_settings, screen, aliens, enemy_fire):
+		super().__init__()
+		self.screen = screen
+
+		self.image = pygame.image.load('images/bullet_enemy.png')
+		self.rect = self.image.get_rect()		
+		enemies = []
+		for alien in aliens:
+			enemies.append(alien)
+			alien = random.choice(enemies)
+		self.rect.centerx = alien.rect.centerx
+		self.rect.bottom = alien.rect.bottom
+
+		self.y = float(self.rect.y)
+
+		self.speed_factor = ai_settings.enemy_bullet_speed_factor
+
+
+	def update(self):
+		self.y += self.speed_factor
+		self.rect.y = self.y
+
+	def draw_enemy_bullet(self):
+		self.screen.blit(self.image, self.rect)
