06dc4c85adee4b14674706e8923a0a62ff9c41b9
Aknur <aknurtemir9@gmail.com>
2020-11-23 17:28:46 +0500

    game

--- me.py
+++ me.py
@@ -7,23 +7,28 @@ class Ship(Sprite):
         self.screen = screen
         self.ai_settings=ai_settings
 
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/spaceship.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
+        # Start new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom - 5
         self.center = float(self.rect.centerx)
-
+        
+        # Movement flag
         self.moving_right = False
         self.moving_left = False
     
     def update(self):
+        # Update the ship's center value not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right - 10:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 10:
             self.center -= self.ai_settings.ship_speed_factor
 
+        # Update rect object from self.center.
         self.rect.centerx = self.center
 
     def center_ship(self):
