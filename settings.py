cc3f6cc457215323ba8b70a994ae33edb5c96bab
Aknur <aknurtemir9@gmail.com>
2020-11-23 17:42:08 +0500

    game

--- settings.py
+++ settings.py
@@ -4,7 +4,11 @@ class Settings():
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 670
+
         self.bg = pygame.image.load('images/space.jpg')
+
+        # Ship settings
+		self.ship_limit = 2
         
         # Bullet settings
         self.bullet_speed_factor = 1
@@ -16,4 +20,19 @@ class Settings():
         self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
         self.fleet_direction = 1
+
+    def initialize_dynamic_settings(self):
+		self.ship_speed_factor = 8
+		self.bullet_speed_factor = 17
+		self.alien_speed_factor = 4
+		self.enemy_bullet_speed_factor = 8
+
+    def increase_speed(self):
+		self.ship_speed_factor *= self.speed_up_scale
+		self.bullet_speed_factor *= self.speed_up_scale
+		self.alien_speed_factor *= self.speed_up_scale
+		self.enemy_bullet_speed_factor *= self.speed_up_scale
+
+
+
 
