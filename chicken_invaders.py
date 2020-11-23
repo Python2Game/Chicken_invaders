bd7f2bac276537cf90075d032c3771c1a73f44f2
Aknur <aknurtemir9@gmail.com>
2020-11-23 17:35:21 +0500

    game

--- chicken_invaders.py
+++ chicken_invaders.py
@@ -6,17 +6,21 @@ from chicken import Chicken
 import pygame
 import game_functions as gf
 from pygame.sprite import Group
+
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Chicken Invaders")
+
+    # Make a ship, a group of bullets, and a group of aliens.
     ship=Ship(ai_settings, screen)
     aliens = Chicken(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    gf.create_fleet(ai_settings, screen,ship, aliens)
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
