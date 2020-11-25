f2561c3486312ca28fda150003034c13e57cfd54
Aknur <aknurtemir9@gmail.com>
2020-11-25 10:52:47 +0500

    game

--- button.py
+++ button.py
@@ -30,3 +30,36 @@ class Button():
 		# Draw blank button and then draw message.
 		self.screen.fill(self.button_color, self.rect)
 		self.screen.blit(self.msg_image, self.msg_image_rect)
+
+class Game_Over():
+	def __init__(self, ai_settings, screen, msg):
+		self.screen = screen
+		self.screen_rect = screen.get_rect()
+
+		# Set the dimensions and properties of the button.
+		self.width, self.height = 400, 150
+		self.button_color = (230, 25, 25)
+		self.text_color = (0, 0, 0)
+		self.font = pygame.font.Font('invasion.TTF', 60)
+
+		# Build the button's rect object and center it.
+		self.rect = pygame.Rect(0, 0, self.width, self.height)
+		self.rect.center = self.screen_rect.center
+		self.rect.centery = self.screen_rect.centery - 75
+
+		# The button message needs to prepped only once
+		self.prep_msg(msg)
+
+		# Flag for game over
+		self.over = False
+
+	def prep_msg(self, msg):
+		self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
+		self.msg_image_rect = self.msg_image.get_rect()
+		self.msg_image_rect.center = self.rect.center
+		self.msg_image_rect.centery = self.rect.centery 
+
+	def draw_button(self):
+		# Draw blank button and then draw message.
+		self.screen.fill(self.button_color, self.rect)
+		self.screen.blit(self.msg_image, self.msg_image_rect)
